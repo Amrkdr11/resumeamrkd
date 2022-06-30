5677d2bfcff9bb1564e813c2c525b789bf29958e
Amrkdr11 <96412622+Amrkdr11@users.noreply.github.com>
2022-06-30 16:59:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Student","Programmer","Duathlete" ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+   
+    
